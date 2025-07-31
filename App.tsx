f8753a4807db655245c6937eb11bc746f0724936
unknown <naledi.lebelo134340@gmail.com>
2025-07-31 22:12:58 +0200

    Commit Message

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
@@ -6,7 +5,6 @@ export default function App() {
     <View style={styles.container}>
        <Text> Hello, Naledi</Text>
        <Text> The only difficulty experienced was navigating the terminals and ensuring that the right steps were taken to ensure that the app would be able to run on an emulator. At some point, I did not open the project in the correct location, so the emulator would not run at all. I resolved this issue by asking the lecturer to fix it for me and explain the issue that I was experiencing </Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
